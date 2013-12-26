74aeedc4adca1926fbdf7fee3dc69cd3839cb235
antitoxic <antitoxic@gmail.com>
2013-12-26 17:47:01 +0200

    Added initial skeleton for Facebook login and registration.

--- app/api.py
+++ app/api.py
@@ -224,9 +224,57 @@ class Types(restful.Resource):
 		return (data, 200) if types else {'message': 'Nothing Found.', 'status': 404}, 404
 
 
+# ref: pythonhosted.org/Flask-OAuth/#facebook-oauth
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=FACEBOOK_APP_ID,
+    consumer_secret=FACEBOOK_APP_SECRET,
+    request_token_params={'scope': 'email'}
+)
+
+
+class Login(restful.Resource):
+
+    def get(self):
+        # ref: pythonhosted.org/Flask-OAuth/#signing-in-authorizing
+        return facebook.authorize(
+            callback=url_for(
+                'auth',
+                next=request.args.get('next') or request.referrer or None
+            ))
+
+
+class Auth(restful.Resource):
+
+    @facebook.authorized_handler
+    def get(self, resp):
+        # ref: pythonhosted.org/Flask-OAuth/#signing-in-authorizing
+        next_url = request.args.get('next') or url_for('index')
+        if resp is None:
+            flash(u'You denied the request to sign in.')
+            return redirect(next_url)
+
+        session['twitter_token'] = (
+            resp['oauth_token'],
+            resp['oauth_token_secret']
+        )
+        session['twitter_user'] = resp['screen_name']
+        # @todo if user doesnt exist - create one
+        # @todo retrieve data for user by email
+
+        flash('You were signed in as %s' % resp['screen_name'])
+        return redirect(next_url)
+
+
+
 api.add_resource(Signal, '/signal/<string:signal_id>', endpoint='signal')
 api.add_resource(Signals, '/signals', endpoint='signals')
 api.add_resource(Types, '/types', endpoint='types')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Auth, '/login/auth', endpoint='auth')
 
 if __name__ == '__main__':
 	app.run(debug=True)
